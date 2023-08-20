f789b23e2f98e2137ff8c6779fc93a51082f574d
Dejana Tesanovic <dejana.tesanovic5@gmail.com>
2023-08-20 13:58:51 +0200

    Added model and LoginPage

---
+++ Project/Project/Model/Apartment.cs
@@ -0,0 +1,10 @@
+﻿namespace Project.Model
+{
+    public class Apartment : BaseModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int RoomsQuantity { get; set; }
+        public int MaxNumberOfGuests { get; set; }
+    }
+}
---
+++ Project/Project/Model/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class BaseModel
+    {
+        public string Id { get; set; }
+        public bool Deleted { get; set; } = false;
+    }
+}
