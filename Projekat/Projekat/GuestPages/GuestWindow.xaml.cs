c05c705b59453225cab882b52fc70f1453490967
Dejana Tesanovic <dejana.tesanovic5@gmail.com>
2023-08-20 21:55:04 +0200

    Added home pages

--- Projekat/Projekat/GuestPages/GuestWindow.xaml.cs
+++ Projekat/Projekat/GuestPages/GuestWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace Projekat.GuestPages
         public GuestWindow(User user)
         {
             InitializeComponent();
+            Name.Text = user.Name;
+            Surname.Text = user.Surname;
         }
     }
 }
