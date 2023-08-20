f1699befb590ad85376d3a5e3e86167d5a3938b8
Dejana Tesanovic <dejana.tesanovic5@gmail.com>
2023-08-20 14:56:48 +0200

    Fixed problems with project path

--- Projekat/Projekat/MainWindow.xaml.cs
+++ Projekat/Projekat/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Project
+namespace Projekat
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- Projekat/Projekat/Properties/AssemblyInfo.cs
+++ Projekat/Projekat/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Project")]
+[assembly: AssemblyTitle("Projekat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Project")]
+[assembly: AssemblyProduct("Projekat")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
